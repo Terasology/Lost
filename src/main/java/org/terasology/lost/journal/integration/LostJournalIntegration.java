b09726c2ef15fc22537f60602ad6d47cdcd24d2f
Nihal Singh <nihal.111@gmail.com>
2017-07-16 18:27:46 +0530

    Journal fix

--- src/main/java/org/terasology/lost/journal/integration/LostJournalIntegration.java
+++ src/main/java/org/terasology/lost/journal/integration/LostJournalIntegration.java
@@ -33,6 +33,7 @@ import org.terasology.journal.JournalManager;
 import org.terasology.journal.TimestampResolver;
 import org.terasology.logic.characters.CharacterComponent;
 import org.terasology.logic.inventory.events.InventorySlotChangedEvent;
+import org.terasology.logic.players.event.OnPlayerSpawnedEvent;
 import org.terasology.registry.In;
 import org.terasology.rendering.nui.HorizontalAlign;
 import org.terasology.rendering.nui.widgets.browser.data.ParagraphData;
@@ -46,8 +47,8 @@ import java.util.Collection;
 import java.util.List;
 
 @RegisterSystem(RegisterMode.AUTHORITY)
-public class ItemPickupJournalIntegration extends BaseComponentSystem {
-    private static final Logger logger = LoggerFactory.getLogger(ItemPickupJournalIntegration.class);
+public class LostJournalIntegration extends BaseComponentSystem {
+    private static final Logger logger = LoggerFactory.getLogger(LostJournalIntegration.class);
     @In
     private JournalManager journalManager;
     @In
@@ -68,6 +69,19 @@ public class ItemPickupJournalIntegration extends BaseComponentSystem {
 
         BrowserJournalChapterHandler chapterHandler = new BrowserJournalChapterHandler();
 
+        chapterHandler.registerJournalEntry("Exploration Log #111",
+                Arrays.asList(
+                        createTitleParagraph("Exploration Log #111"),
+                        createTextParagraph("Survey of Epsilon Draconis system is now complete. The system is concluded to be inhabitable " +
+                                "after seven months of relentless exploration. However, planet 7: Scorpion Chapel, scouted by Ranger 729 is " +
+                                "confirmed to possess huge amounts of metal ores and methane. The planet can be scavenged for resources but is " +
+                                "still incapable for supporting life akin to other planets in the system."),
+                        createTextParagraph("Iota Lateralus, a binary star planetary system, distant 3006 light years from Narvij, " +
+                                "appears promising. Survey is about to begin in two days. Inspection of circumbinary planet 4 of the" +
+                                " system- Rosseta Parabole, has been assigned to me. Mode of travel would be a regular triodal wormhole. " +
+                                "Duration should be less than a month. Glad to be back scouting!<l><l>")
+                ));
+
         chapterHandler.registerJournalEntry("Exploration Log #112",
                 Arrays.asList(
                         createTitleParagraph("Exploration Log #112"),
@@ -77,7 +91,19 @@ public class ItemPickupJournalIntegration extends BaseComponentSystem {
                                 " I believe it was impacted in the journey. With the wormhole creator destroyed, I have no way to go back home."),
                         createTextParagraph("The planet seems promising, as of yet. There is no sign of intelligent life. I do see some alien " +
                                 "lifeform alike some of the animals back home. I should find some way to ensure survival. This might just be " +
-                                "the last planet I set foot on.")
+                                "the last planet I set foot on.<l><l>")
+                ));
+
+        chapterHandler.registerJournalEntry("Exploration Log #112",
+                Arrays.asList(
+                        createTitleParagraph("Exploration Log #112"),
+                        createTextParagraph("After five hours of trying to establish communication with base back at Narvij and nearby space " +
+                                "stations, I’ve given up. The solar flares from Proxima Lateralus seem to have some sort of interference with" +
+                                " the signals. The situation seems grim, my Antrum Sabre, which I used to create the wormhole, is now dysfunctional." +
+                                " I believe it was impacted in the journey. With the wormhole creator destroyed, I have no way to go back home."),
+                        createTextParagraph("The planet seems promising, as of yet. There is no sign of intelligent life. I do see some alien " +
+                                "lifeform alike some of the animals back home. I should find some way to ensure survival. This might just be " +
+                                "the last planet I set foot on.<l><l>")
                 ));
 
         chapterHandler.registerJournalEntry("Exploration Log #113",
@@ -96,7 +122,7 @@ public class ItemPickupJournalIntegration extends BaseComponentSystem {
                                 "civilization more advanced than ours? Why don’t I see anyone around? Did they get wiped out? Was it a natural " +
                                 "calamity or did they bring a catastrophe onto themselves?<l><l>" +
                                 "Why do they refer to the treasure as one that would bring misery? Is the treasure the reason for the collapse " +
-                                "of the early civilization that existed here?<l>")
+                                "of the early civilization that existed here?<l><l>")
                 ));
 
         chapterHandler.registerJournalEntry("Exploration Log- The Circle Of Life",
@@ -110,7 +136,7 @@ public class ItemPickupJournalIntegration extends BaseComponentSystem {
                                 "Mistime a jump and you’re cooked meat. The treasure chest possessed the key- an Arkenstone. Along with the " +
                                 "artifact, was also another book. The book says that this small stone is the most integral part of the Portal.<l><l>" +
                                 "I’m starting to believe that the whole idea might actually be true. That the early civilization did have a " +
-                                "piece of witchcraft and wizardry that allowed them to stitch a hole in the fabric of space and time.<l>")
+                                "piece of witchcraft and wizardry that allowed them to stitch a hole in the fabric of space and time.<l><l>")
                 ));
 
         chapterHandler.registerJournalEntry("Exploration Log- Shattered Temple of Hope",
@@ -122,7 +148,7 @@ public class ItemPickupJournalIntegration extends BaseComponentSystem {
                                 "has reached a new high. The tunnel led to a temple that possessed another chest at its end. " +
                                 "The path to the treasure was however blocked by a series of fireball launchers and swinging blades. " +
                                 "The treasure chest consisted of Shattered Plasma the element for the construction of the foundation of the portal.<l><l>" +
-                                "I wonder how the portal might actually work. Would it be like my Antrum Sabre that became dysfunctional?<l>")
+                                "I wonder how the portal might actually work. Would it be like my Antrum Sabre that became dysfunctional?<l><l>")
                 ));
 
         chapterHandler.registerJournalEntry("Exploration Log- The Hunt for Truth",
@@ -136,18 +162,19 @@ public class ItemPickupJournalIntegration extends BaseComponentSystem {
                                 "series of swinging blades that blocked the way.<l><l>" +
                                 "Made with such precision, that the seeker would need to time the sprint perfectly in order " +
                                 "to reach the other side. Moreover, the return was more difficult than the approach.<l><l>" +
-                                "The treasure consisted of the Facade of Truth, one of the three elements of the portal and instructions on how to use it.<l>")
+                                "The treasure consisted of the Facade of Truth, one of the three elements of the portal and instructions on how to use it.<l><l>")
                 ));
 
         chapterHandler.registerJournalEntry("Exploration Log #114",
                 Arrays.asList(
                         createTitleParagraph("Exploration Log #114"),
-                        createTextParagraph("All the items for the portal have been found. Only thing that remains is to put everything together, and fire it up.<l>")
+                        createTextParagraph("All the items for the portal have been found. Only thing that remains is to put everything together, and fire it up.<l><l>")
                 ));
 
         journalManager.registerJournalChapter(lostChapterId,
                 Assets.getTextureRegion("Lost:journalIcons#WoodAndStone").get(),
                 "Lost", chapterHandler);
+        logger.info("registered journal chapter");
     }
 
     private JournalEntryProducer createTimestampEntryProducer(String text) {
@@ -171,6 +198,11 @@ public class ItemPickupJournalIntegration extends BaseComponentSystem {
     }
 
     @ReceiveEvent
+    public void playerSpawned(OnPlayerSpawnedEvent event, EntityRef player) {
+        player.send(new DiscoveredNewJournalEntry(lostChapterId, "Exploration Log #111"));
+    }
+
+    @ReceiveEvent
     public void playerPickedUpItem(InventorySlotChangedEvent event, EntityRef player,
                                    CharacterComponent characterComponent) {
         Prefab prefab = event.getNewItem().getParentPrefab();
@@ -186,21 +218,28 @@ public class ItemPickupJournalIntegration extends BaseComponentSystem {
                 sendOverallJourneyEvent(player);
             } else if (prefabUri.equals(new ResourceUrn("Lost", "facadeOfTruthBook"))) {
                 player.send(new DiscoveredNewJournalEntry(lostChapterId, "Exploration Log- The Hunt for Truth"));
+                sendOverallJourneyEvent(player);
             } else if (prefabUri.equals(new ResourceUrn("Lost", "shatteredPlasmaBook"))) {
                 player.send(new DiscoveredNewJournalEntry(lostChapterId, "Exploration Log- Shattered Temple of Hope"));
+                sendOverallJourneyEvent(player);
             }
         }
     }
 
     private void sendOverallJourneyEvent(EntityRef player) {
         JournalAccessComponent journalAccessComponent = player.getComponent(JournalAccessComponent.class);
-        List<String> lostJournalEntries = journalAccessComponent.discoveredJournalEntries.get("Lost");
-        if (lostJournalEntries.contains("Exploration Log- The Circle Of Life") &&
-                lostJournalEntries.contains("Exploration Log- The Hunt for Truth") &&
-                lostJournalEntries.contains("Exploration Log- Shattered Temple of Hope")) {
-            player.send(new DiscoveredNewJournalEntry(lostChapterId, "Exploration Log- The Circle Of Life"));
+        List<String> lostJournalEntries = journalAccessComponent.discoveredJournalEntries.get(lostChapterId);
+        int flag = 0;
+        for (String lostJournalEntry : lostJournalEntries) {
+            if (lostJournalEntry.contains("Exploration Log- The Circle Of Life") ||
+                    lostJournalEntry.contains("Exploration Log- The Hunt for Truth") ||
+                    lostJournalEntry.contains("Exploration Log- Shattered Temple of Hope")) {
+                flag += 1;
+            }
+
+            if (flag == 3) {
+                player.send(new DiscoveredNewJournalEntry(lostChapterId, "Exploration Log #114"));
+            }
         }
     }
-
-
 }
