fd3e1fa8dbf021375c0f4a3b36093f8b7a26bfc8
meetcshah19 <meetcshah19@gmail.com>
2020-06-21 20:51:37 +0530

    upload changes

--- src/main/java/org/terasology/lost/OnSpawnSystem.java
+++ src/main/java/org/terasology/lost/OnSpawnSystem.java
@@ -25,6 +25,25 @@ import org.terasology.logic.inventory.InventoryComponent;
 import org.terasology.logic.inventory.InventoryManager;
 import org.terasology.logic.players.event.OnPlayerSpawnedEvent;
 import org.terasology.registry.In;
+import org.terasology.biomesAPI.OnBiomeChangedEvent;
+import org.terasology.logic.console.Console;
+import org.terasology.logic.location.LocationComponent;
+import org.terasology.logic.spawner.FixedSpawner;
+import org.terasology.math.Region3i;
+import org.terasology.math.geom.ImmutableVector2f;
+import org.terasology.math.geom.Vector2f;
+import org.terasology.math.geom.Vector2i;
+import org.terasology.math.geom.Vector3f;
+import org.terasology.math.geom.Vector3i;
+import org.terasology.polyworld.biome.WhittakerBiome;
+import org.terasology.polyworld.biome.BiomeModel;
+import org.terasology.polyworld.graph.Graph;
+import org.terasology.polyworld.graph.GraphFacet;
+import org.terasology.polyworld.graph.GraphFacetProvider;
+import org.terasology.polyworld.biome.WhittakerBiomeModelFacet;
+import org.terasology.world.generation.Region;
+import org.terasology.lost.generator.*;
+import org.terasology.math.geom.ImmutableVector2f;
 
 @RegisterSystem(RegisterMode.AUTHORITY)
 public class OnSpawnSystem extends BaseComponentSystem {
@@ -32,9 +51,49 @@ public class OnSpawnSystem extends BaseComponentSystem {
     private EntityManager entityManager;
     @In
     private InventoryManager inventoryManager;
+    @In
+    private Console console;
 
     @ReceiveEvent
     public void onPlayerSpawn(OnPlayerSpawnedEvent event, EntityRef player, InventoryComponent inventory) {
         inventoryManager.giveItem(player, null, entityManager.create("Lost:antrumSabre"));
+    }
+
+    @ReceiveEvent
+    public void onBiomeChange(OnBiomeChangedEvent event, EntityRef player) {
+        console.addMessage("OLD:"+event.getOldBiome().getDisplayName());
+        console.addMessage("NEW:"+event.getNewBiome().getDisplayName());
+        LocationComponent loc = player.getComponent(LocationComponent.class);
+        Vector3f pos = loc.getWorldPosition();
+
+        int searchRadius = 0;
+        Vector3i ext = new Vector3i(searchRadius, 0, searchRadius);
+        Vector3i desiredPos = new Vector3i(pos.getX(), 1, pos.getZ());
+
+        // try and find somewhere in this region a spot to land
+        Region3i spawnArea = Region3i.createFromCenterExtents(desiredPos, ext);
+        console.addMessage(spawnArea.size().toString());
+        Region worldRegion = LostWorldGenerator.world.getWorldData(spawnArea);
+
+        GraphFacet graphs = worldRegion.getFacet(GraphFacet.class);
+        WhittakerBiomeModelFacet model = worldRegion.getFacet(WhittakerBiomeModelFacet.class);
+        Vector2f pos2d = new Vector2f(pos.getX(), pos.getZ());
+        double d = 1000000;
+        ImmutableVector2f center = null;
+        for (Graph g : graphs.getAllGraphs()) {
+            BiomeModel biomeModel = model.get(g);
+            for (org.terasology.polyworld.graph.Region r : g.getRegions()) {
+                WhittakerBiome biome = biomeModel.getBiome(r);
+                if(biome.getDisplayName().contains(event.getNewBiome().getDisplayName())){
+                    double temp = Math.pow(pos2d.x-pos.getX(),2)+Math.pow(pos2d.y-pos.getZ(),2);
+                    if(temp<d){
+                        d=temp;
+                        center=r.getCenter();
+                    }
+                }
+
+            }
+        }
+        console.addMessage(center.toString());
     }
 }
--- src/main/java/org/terasology/lost/generator/LostWorldGenerator.java
+++ src/main/java/org/terasology/lost/generator/LostWorldGenerator.java
@@ -15,42 +15,46 @@
  */
 package org.terasology.lost.generator;
 
-import com.google.common.base.Function;
-import com.google.common.base.Predicate;
-import org.terasology.anotherWorld.AnotherWorldBiomes;
-import org.terasology.anotherWorld.ChunkDecorator;
-import org.terasology.anotherWorld.FeatureGenerator;
-import org.terasology.anotherWorld.decorator.BeachDecorator;
-import org.terasology.anotherWorld.decorator.BiomeDecorator;
-import org.terasology.anotherWorld.decorator.BlockCollectionPredicate;
-import org.terasology.anotherWorld.decorator.layering.DefaultLayersDefinition;
-import org.terasology.anotherWorld.decorator.layering.LayeringConfig;
-import org.terasology.anotherWorld.decorator.layering.LayeringDecorator;
-import org.terasology.anotherWorld.decorator.ore.OreDecorator;
-import org.terasology.anotherWorld.generation.*;
-import org.terasology.anotherWorld.util.PDist;
-import org.terasology.anotherWorld.util.Provider;
-import org.terasology.anotherWorld.util.alpha.IdentityAlphaFunction;
-import org.terasology.anotherWorld.util.alpha.MinMaxAlphaFunction;
-import org.terasology.anotherWorld.util.alpha.PowerAlphaFunction;
-import org.terasology.anotherWorld.util.alpha.UniformNoiseAlpha;
-import org.terasology.climateConditions.ClimateConditionsSystem;
-import org.terasology.climateConditions.ConditionsBaseField;
 import org.terasology.core.world.generator.facetProviders.SeaLevelProvider;
-import org.terasology.core.world.generator.facetProviders.SurfaceToDensityProvider;
+import org.terasology.core.world.generator.rasterizers.FloraRasterizer;
+import org.terasology.core.world.generator.rasterizers.TreeRasterizer;
 import org.terasology.engine.SimpleUri;
-import org.terasology.gf.generator.*;
+import org.terasology.entitySystem.entity.EntityRef;
+import org.terasology.logic.location.LocationComponent;
+import org.terasology.logic.spawner.FixedSpawner;
+import org.terasology.math.Region3i;
+import org.terasology.math.geom.ImmutableVector2f;
+import org.terasology.math.geom.Vector2f;
+import org.terasology.math.geom.Vector2i;
+import org.terasology.math.geom.Vector3f;
+import org.terasology.math.geom.Vector3i;
+import org.terasology.polyworld.biome.BiomeModel;
+import org.terasology.polyworld.biome.WhittakerBiome;
+import org.terasology.polyworld.biome.WhittakerBiomeModelFacet;
+import org.terasology.polyworld.biome.WhittakerBiomeModelProvider;
+import org.terasology.polyworld.biome.WhittakerBiomeProvider;
+import org.terasology.polyworld.elevation.ElevationModelFacetProvider;
+import org.terasology.polyworld.elevation.ElevationProvider;
+import org.terasology.polyworld.elevation.FlatLakeProvider;
+import org.terasology.polyworld.flora.FloraProvider;
+import org.terasology.polyworld.flora.TreeProvider;
+import org.terasology.polyworld.graph.Graph;
+import org.terasology.polyworld.graph.GraphFacet;
+import org.terasology.polyworld.graph.GraphFacetProvider;
+import org.terasology.polyworld.moisture.MoistureModelFacetProvider;
+import org.terasology.polyworld.raster.RiverRasterizer;
+import org.terasology.polyworld.raster.WhittakerRasterizer;
+import org.terasology.polyworld.rivers.RiverModelFacetProvider;
+import org.terasology.polyworld.rp.WorldRegionFacetProvider;
+import org.terasology.polyworld.water.WaterModelFacetProvider;
 import org.terasology.registry.CoreRegistry;
-import org.terasology.registry.In;
-import org.terasology.world.block.Block;
-import org.terasology.world.block.BlockManager;
 import org.terasology.world.generation.BaseFacetedWorldGenerator;
-import org.terasology.world.generation.FacetProvider;
+import org.terasology.world.generation.Region;
 import org.terasology.world.generation.WorldBuilder;
+import org.terasology.world.generation.World;
 import org.terasology.world.generator.RegisterWorldGenerator;
-import org.terasology.world.generator.WorldConfigurator;
-import org.terasology.world.generator.WorldConfiguratorAdapter;
 import org.terasology.world.generator.plugin.WorldGeneratorPluginLibrary;
+import org.terasology.world.viewer.picker.CirclePickerClosest;
 
 import java.util.Arrays;
 import java.util.LinkedList;
@@ -58,268 +62,72 @@ import java.util.List;
 
 @RegisterWorldGenerator(id = "lost", displayName = "Lost", description = "Generates the world for playing the 'Lost' exploration world.")
 public class LostWorldGenerator extends BaseFacetedWorldGenerator {
-    private BlockManager blockManager;
-
-    @In
-    private WorldGeneratorPluginLibrary worldGeneratorPluginLibrary;
-
-    private List<ChunkDecorator> chunkDecorators = new LinkedList<>();
-    private List<FeatureGenerator> featureGenerators = new LinkedList<>();
-    private List<FacetProvider> facetProviders = new LinkedList<>();
-
-    private int seaLevel = 1700;
-    private int maxLevel = 2300;
-    private float biomeDiversity = 0.5f;
-
-    private Function<Float, Float> temperatureFunction = IdentityAlphaFunction.singleton();
-    private Function<Float, Float> humidityFunction = IdentityAlphaFunction.singleton();
-
-    private PerlinSurfaceHeightProvider surfaceHeightProvider;
-
+    public static World world;
     public LostWorldGenerator(SimpleUri uri) {
         super(uri);
     }
 
-    public void addChunkDecorator(ChunkDecorator chunkGenerator) {
-        chunkDecorators.add(chunkGenerator);
-    }
-
-    public void addFeatureGenerator(FeatureGenerator featureGenerator) {
-        featureGenerators.add(featureGenerator);
-    }
-
-    public void addFacetProvider(FacetProvider facetProvider) {
-        facetProviders.add(facetProvider);
-    }
-
-    public void setSeaLevel(int seaLevel) {
-        this.seaLevel = seaLevel;
-    }
-
-    public void setMaxLevel(int maxLevel) {
-        this.maxLevel = maxLevel;
-    }
-
-    /**
-     * 0=changing slowly, 1=changing frequently
-     *
-     * @param biomeDiversity
-     */
-    public void setBiomeDiversity(float biomeDiversity) {
-        this.biomeDiversity = biomeDiversity;
-    }
-
-    public void setTemperatureFunction(Function<Float, Float> temperatureFunction) {
-        this.temperatureFunction = temperatureFunction;
-    }
-
-    public void setHumidityFunction(Function<Float, Float> humidityFunction) {
-        this.humidityFunction = humidityFunction;
-    }
-
-    public void setLandscapeOptions(float seaFrequency, float terrainDiversity, Function<Float, Float> generalTerrainFunction,
-                                    Function<Float, Float> heightBelowSeaLevelFunction,
-                                    Function<Float, Float> heightAboveSeaLevelFunction,
-                                    float hillinessDiversity, Function<Float, Float> hillynessFunction) {
-        surfaceHeightProvider = new PerlinSurfaceHeightProvider(seaFrequency, terrainDiversity, generalTerrainFunction,
-                heightBelowSeaLevelFunction,
-                heightAboveSeaLevelFunction,
-                hillinessDiversity, hillynessFunction, seaLevel, maxLevel);
-    }
-
-    @Override
-    public void initialize() {
-        setupRasterizerInitializer();
-
-        getWorld().initialize();
-    }
-
-    private void setupRasterizerInitializer() {
-        addChunkDecorator(new BiomeDecorator());
-
-        blockManager = CoreRegistry.get(BlockManager.class);
-
-        final Block mantle = blockManager.getBlock("CoreAssets:MantleStone");
-        final Block stone = blockManager.getBlock("CoreAssets:Stone");
-        final Block water = blockManager.getBlock("CoreAssets:Water");
-        final Block sand = blockManager.getBlock("CoreAssets:Sand");
-        final Block clay = blockManager.getBlock("Woodcrafting:ClayStone");
-        final Block dirt = blockManager.getBlock("CoreAssets:Dirt");
-        final Block grass = blockManager.getBlock("CoreAssets:Grass");
-        final Block snow = blockManager.getBlock("CoreAssets:Snow");
-        final Block ice = blockManager.getBlock("CoreAssets:Ice");
-
-        // Setup biome terrain layers
-        setupLayers(mantle, water, stone, sand, dirt, grass, snow, ice, seaLevel);
-
-        // Replace stone with sand on the sea shores
-        addChunkDecorator(
-                new BeachDecorator(new BlockCollectionPredicate(Arrays.asList(stone, dirt, grass, snow)), new BeachBlockProvider(0.05f, clay, sand), seaLevel - 5, seaLevel + 2));
-
-        Predicate<Block> removableBlocks = new BlockCollectionPredicate(Arrays.asList(stone, sand, dirt, grass, snow));
-
-        // Dig some caves in the terrain
-//        addChunkDecorator(
-//                new CaveDecorator(getWorldSeed().hashCode(), removableBlocks, new PDist(0.2f, 0f), new PDist(5f, 1f),
-//                        new PDist(1750f, 400f), new PDist(50f, 10f), new PDist(2f, 0.5f), blockManager));
-
-        // Setup ore spawning
-        setupOreGenerator(stone);
-
-        FloraFeatureGenerator floraDecorator = new FloraFeatureGenerator();
-        addFeatureGenerator(floraDecorator);
-
-        if (worldBuilder == null) {
-            worldBuilder = createWorld();
-        }
-
-        for (ChunkDecorator chunkDecorator : chunkDecorators) {
-            worldBuilder.addRasterizer(chunkDecorator);
-        }
-        for (FeatureGenerator featureGenerator : featureGenerators) {
-            worldBuilder.addRasterizer(featureGenerator);
-        }
-    }
-
-    protected void setupGenerator() {
-
-        setSeaLevel(seaLevel);
-        setMaxLevel(maxLevel);
-
-        // Make sure that area on the sea level is not dry, this will prevent deserts spawning next to sea
-        setHumidityFunction(
-                new MinMaxAlphaFunction(new UniformNoiseAlpha(IdentityAlphaFunction.singleton()), 0.3f, 1f));
-        // Make sure that area on the sea level in not too cold, so that colder areas (with snow) will
-        // only exist in higher Y-levels
-        setTemperatureFunction(
-                new MinMaxAlphaFunction(new UniformNoiseAlpha(IdentityAlphaFunction.singleton()), 0.4f, 1f));
-
-        setLandscapeOptions(
-                // 40% of the landscape is under water
-                0.4f,
-                // Semi high continent size
-                0.1f,
-                // Height is distributed Gaussian
-                IdentityAlphaFunction.singleton(),
-                // Terrain underwater is more shallow than deep (PowerAlphaFunction) and also at least 0.3*seaLevel height
-                new MinMaxAlphaFunction(new PowerAlphaFunction(IdentityAlphaFunction.singleton(), 0.7f), 0.3f, 1f),
-                // Make the lowlands a bit more common than higher areas (using PowerAlphaFunction)
-                new PowerAlphaFunction(IdentityAlphaFunction.singleton(), 2f),
-                // Smoothen the terrain a bit
-                0.5f, new MinMaxAlphaFunction(new PowerAlphaFunction(new UniformNoiseAlpha(IdentityAlphaFunction.singleton()), 1.3f), 0.1f, 1f));
-
-        // Setup flora growing in the world
-        setupFlora(seaLevel);
-    }
-
     @Override
     protected WorldBuilder createWorld() {
-        setupGenerator();
-
-        ClimateConditionsSystem environmentSystem = new ClimateConditionsSystem();
-        environmentSystem.setWorldSeed(getWorldSeed());
-        environmentSystem.configureHumidity(seaLevel, maxLevel, biomeDiversity, humidityFunction, 0, 1);
-        environmentSystem.configureTemperature(seaLevel, maxLevel, biomeDiversity, temperatureFunction, -20, 40);
-
-        ConditionsBaseField temperatureBaseField = environmentSystem.getTemperatureBaseField();
-        ConditionsBaseField humidityBaseField = environmentSystem.getHumidityBaseField();
-
-        WorldBuilder worldBuilder = new WorldBuilder(worldGeneratorPluginLibrary);
-        worldBuilder.addProvider(new BiomeProvider());
-        worldBuilder.addProvider(new HillynessProvider());
-        worldBuilder.addProvider(surfaceHeightProvider);
-        worldBuilder.addProvider(new SurfaceToDensityProvider());
-        worldBuilder.addProvider(new HumidityProvider(humidityBaseField));
-        worldBuilder.addProvider(new TemperatureProvider(temperatureBaseField));
-        worldBuilder.addProvider(new TerrainVariationProvider());
-        worldBuilder.addProvider(new SeaLevelProvider(seaLevel));
-
-        for (FacetProvider facetProvider : facetProviders) {
-            worldBuilder.addProvider(facetProvider);
-        }
-
-        return worldBuilder;
+        int maxCacheSize = 20;
+        return new WorldBuilder(CoreRegistry.get(WorldGeneratorPluginLibrary.class))
+                .setSeaLevel(6)
+                .addProvider(new SeaLevelProvider(6))
+                .addProvider(new WorldRegionFacetProvider(maxCacheSize))
+                .addProvider(new GraphFacetProvider(maxCacheSize,2f,3))
+                .addProvider(new WaterModelFacetProvider(maxCacheSize))
+                .addProvider(new ElevationModelFacetProvider(maxCacheSize))
+                .addProvider(new ElevationProvider())
+                .addProvider(new RiverModelFacetProvider(maxCacheSize))
+                .addProvider(new FlatLakeProvider())
+                .addProvider(new MoistureModelFacetProvider(maxCacheSize))
+                .addProvider(new WhittakerBiomeModelProvider(maxCacheSize))
+                .addProvider(new WhittakerBiomeProvider())
+                .addProvider(new TreeProvider())
+                .addProvider(new FloraProvider())
+                .addRasterizer(new WhittakerRasterizer())
+                .addRasterizer(new RiverRasterizer())
+                .addRasterizer(new TreeRasterizer())
+                .addRasterizer(new FloraRasterizer());
+//                .addPlugins();
     }
 
     @Override
-    public WorldConfigurator getConfigurator() {
-        return new WorldConfiguratorAdapter();
-    }
+    public Vector3f getSpawnPosition(EntityRef entity) {
+        LocationComponent loc = entity.getComponent(LocationComponent.class);
+        Vector3f pos = loc.getWorldPosition();
 
-    private void setupFlora(int seaLevel) {
-        addFacetProvider(new FloraProvider(seaLevel));
+        int searchRadius = 16;
+        Vector3i ext = new Vector3i(searchRadius, 1, searchRadius);
+        Vector3i desiredPos = new Vector3i(pos.getX(), 1, pos.getZ());
 
-        // new PDist(2f, 0.4f)
-        addFacetProvider(new TreeProvider(1.2f / (16 * 16)));
-        // new PDist(20f, 0.6f)
-        addFacetProvider(new BushProvider(10.3f / (16 * 16)));
-        // new PDist(160f, 40f)
-        addFacetProvider(new FoliageProvider(100f / (16 * 16)));
-    }
+        // try and find somewhere in this region a spot to land
+        Region3i spawnArea = Region3i.createFromCenterExtents(desiredPos, ext);
+        Region worldRegion = getWorld().getWorldData(spawnArea);
+        world=getWorld();
+        GraphFacet graphs = worldRegion.getFacet(GraphFacet.class);
+        WhittakerBiomeModelFacet model = worldRegion.getFacet(WhittakerBiomeModelFacet.class);
+        Vector2f pos2d = new Vector2f(pos.getX(), pos.getZ());
+        CirclePickerClosest<org.terasology.polyworld.graph.Region> picker = new CirclePickerClosest<>(pos2d);
 
-
-    private void setupOreGenerator(Block stone) {
-        Predicate<Block> replacedBlocks = new BlockCollectionPredicate(stone);
-        OreDecorator oreDecorator = new OreDecorator(getWorldSeed().hashCode(), replacedBlocks);
-
-        // Use plugin mechanism to setup required ores for the modules, by default WoodAndStone requires no
-        // ores
-
-        addChunkDecorator(oreDecorator);
-    }
-
-    private void setupLayers(Block mantle, Block sea, Block stone, Block sand, Block dirt, Block grass, Block snow, Block ice,
-                             int seaLevel) {
-        LayeringConfig config = new LayeringConfig(mantle, stone, sea);
-
-        LayeringDecorator layering = new LayeringDecorator(config, getWorldSeed().hashCode());
-
-        DefaultLayersDefinition desertDef = new DefaultLayersDefinition(seaLevel, AnotherWorldBiomes.DESERT.getId());
-        desertDef.addLayerDefinition(new PDist(3, 1), sand, false);
-        desertDef.addLayerDefinition(new PDist(4, 2), dirt, true);
-        layering.addBiomeLayers(desertDef);
-
-        DefaultLayersDefinition forestDef = new DefaultLayersDefinition(seaLevel, AnotherWorldBiomes.FOREST.getId());
-        DefaultLayersDefinition plainsDef = new DefaultLayersDefinition(seaLevel, AnotherWorldBiomes.PLAINS.getId());
-        forestDef.addLayerDefinition(new PDist(1, 0), grass, false);
-        plainsDef.addLayerDefinition(new PDist(1, 0), grass, false);
-        forestDef.addLayerDefinition(new PDist(4, 2), dirt, true);
-        plainsDef.addLayerDefinition(new PDist(4, 2), dirt, true);
-        layering.addBiomeLayers(forestDef);
-        layering.addBiomeLayers(plainsDef);
-
-        DefaultLayersDefinition tundraDef = new DefaultLayersDefinition(seaLevel, AnotherWorldBiomes.TUNDRA.getId());
-        DefaultLayersDefinition taigaDef = new DefaultLayersDefinition(seaLevel, AnotherWorldBiomes.TAIGA.getId());
-        tundraDef.addLayerDefinition(new PDist(1, 0), snow, false);
-        taigaDef.addLayerDefinition(new PDist(1, 0), snow, false);
-        layering.addBiomeLayers(tundraDef);
-        layering.addBiomeLayers(taigaDef);
-
-        DefaultLayersDefinition alpineDef = new DefaultLayersDefinition(seaLevel, AnotherWorldBiomes.ALPINE.getId());
-        alpineDef.addLayerDefinition(new PDist(2f, 1f), ice, false);
-        alpineDef.addLayerDefinition(new PDist(1f, 0f), snow, false);
-        layering.addBiomeLayers(alpineDef);
-
-        DefaultLayersDefinition cliffDef = new DefaultLayersDefinition(seaLevel, AnotherWorldBiomes.CLIFF.getId());
-        layering.addBiomeLayers(cliffDef);
-
-        addChunkDecorator(layering);
-    }
-
-    private final class BeachBlockProvider implements Provider<Block> {
-        private float chance;
-        private Block block1;
-        private Block block2;
-
-        public BeachBlockProvider(float chance, Block block1, Block block2) {
-            this.chance = chance;
-            this.block1 = block1;
-            this.block2 = block2;
+        for (Graph g : graphs.getAllGraphs()) {
+            BiomeModel biomeModel = model.get(g);
+            for (org.terasology.polyworld.graph.Region r : g.getRegions()) {
+                WhittakerBiome biome = biomeModel.getBiome(r);
+                if (!biome.equals(WhittakerBiome.OCEAN) && !biome.equals(WhittakerBiome.LAKE)) {
+                    picker.offer(r.getCenter(), r);
+                }
+            }
+        }
+        Vector2i target;
+        if (picker.getClosest() != null) {
+            ImmutableVector2f hit = picker.getClosest().getCenter();
+            target = new Vector2i(hit.getX(), hit.getY());
+        } else {
+            target = new Vector2i(desiredPos.getX(), desiredPos.getZ());
         }
 
-        @Override
-        public Block provide(float randomValue) {
-            return (randomValue < chance) ? block1 : block2;
-        }
+        FixedSpawner spawner = new FixedSpawner(target.getX(), target.getY());
+        return spawner.getSpawnPosition(getWorld(), entity);
     }
 }
