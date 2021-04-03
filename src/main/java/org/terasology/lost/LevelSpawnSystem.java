ca3ede529b2e7165aa6b9231769d37007b39080b
Michael Pollind <mpollind@gmail.com>
2021-04-03 08:53:04 -0700

    chore: migrate gestalt v7
     PR: https://github.com/MovingBlocks/Terasology/pull/4593

--- src/main/java/org/terasology/lost/LevelSpawnSystem.java
+++ src/main/java/org/terasology/lost/LevelSpawnSystem.java
@@ -9,7 +9,7 @@ import org.joml.Vector3f;
 import org.joml.Vector3i;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.terasology.assets.management.AssetManager;
+import org.terasology.gestalt.assets.management.AssetManager;
 import org.terasology.biomesAPI.OnBiomeChangedEvent;
 import org.terasology.engine.entitySystem.entity.EntityBuilder;
 import org.terasology.engine.entitySystem.entity.EntityManager;
